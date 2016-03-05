7c82420da2b0dc6c65dcf4dec27b4e1c2df41f18
amybohbeanii <jiaxuan@brandeis.edu>
2016-03-05 02:27:49 +0000

    somewhat buggy totalboundaries code

--- ECIComposer/Assets/Scripts/AssetManager.cs
+++ ECIComposer/Assets/Scripts/AssetManager.cs
@@ -10,6 +10,10 @@ public class AssetManager : MonoBehaviour {
 	ObjectSelector objSelector;
 	ObjectList objList;
 	
+	
+	private Bounds totalBounds;
+	public List<GameObject> targets = new List<GameObject>();
+	
 	// Use this for initialization
 	void Start () {
 		objSelector = GameObject.Find ("ObjectSelector").GetComponent ("ObjectSelector") as ObjectSelector;
@@ -18,7 +22,7 @@ public class AssetManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 
 	public void InstantiateObject (string objName) {
@@ -28,14 +32,37 @@ public class AssetManager : MonoBehaviour {
 		go.SetActive (true);
 		go.name = go.name.Replace ("(Clone)", "");
 
+		targets.Add (go);
+
 		foreach (Transform child in go.GetComponentsInChildren<Transform>()) {
-			if (child.gameObject.GetComponent<MeshFilter>() != null) {
+			if (child.gameObject.GetComponent<MeshFilter> () != null) {
 				//Debug.Log (child.name);
 				BoxCollider collider = child.gameObject.AddComponent<BoxCollider> ();
 				Bounds bounds = Helper.Helper.GetObjectBounds (child.gameObject);
 				//Debug.Log (bounds.size);
 				collider.center = bounds.center;
 				collider.size = bounds.size;
+			
+
+				//Calculates total bounds using targets list aka all gameobjects added
+				bool isReset = false;
+				foreach (GameObject gameObject in targets) {
+					if (isReset == false) { 
+						totalBounds = new Bounds(gameObject.transform.position, new Vector3(3,3,3));
+						isReset = true;
+					}
+					totalBounds.Encapsulate(gameObject.transform.position);
+					totalBounds.Encapsulate(bounds);
+				} 
+
+				//variables needed to calculate distance of camera from bounds center
+				Vector3 max = totalBounds.size;
+				float radius = Mathf.Max(max.x, Mathf.Max(max.y, max.z));
+				float distance = radius / (Mathf.Sin(Camera.main.fieldOfView * Mathf.Deg2Rad / 2f));
+
+				//points camera at center of total bounds
+				Camera.main.transform.position = totalBounds.center;
+				Camera.main.transform.Translate (0 ,0, -distance); 
 			}
 		}
 
